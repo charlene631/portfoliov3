08c15b90f11bd4afb600f23adae81bdf28e0ed5a
Charlène <charlene.dev2025@gmail.com>
2026-03-17 16:01:56 +0100

    ajout style stack et optimisation ux

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -4,7 +4,16 @@ export default function About() {
   return (
     <section className="section" id="about">
       <h2>À propos de moi</h2>
-      <p>Je construis des interfaces claires et fonctionnelles, orientée sécurité, responsive et navigation agréable.</p>
+      <p>Je développe des applications web full stack avec React et Node.js, en portant une attention particulière à la clarté des interfaces, à la sécurité et à l’expérience utilisateur.</p>
+      <div className="stack">
+  <img src="/icons/react.svg" alt="React" />
+  <img src="/icons/node.svg" alt="Node" />
+  <img src="/icons/mysql.svg" alt="MySQL" />
+  <img src="/icons/javascript.svg" alt="JavaScript" />
+  <img src="/icons/git.svg" alt="Git" />
+  <img src="/icons/postman.svg" alt="Postman" />
+  <img src="/icons/ubuntu.svg" alt="Ubuntu" />
+</div>
     </section>
   );
 }
