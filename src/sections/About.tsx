8d3a79ff10be1b9cb8128a780b66f1264fb6e9bd
Charlène <charlene.dev2025@gmail.com>
2026-03-18 11:14:21 +0100

    ajout icons

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -9,10 +9,14 @@ export default function About() {
   <img src="/icons/react.svg" alt="React" />
   <img src="/icons/node.svg" alt="Node" />
   <img src="/icons/mysql.svg" alt="MySQL" />
+  <img src="/icons/mongodb.svg" alt="MongoDB" />
   <img src="/icons/javascript.svg" alt="JavaScript" />
   <img src="/icons/git.svg" alt="Git" />
   <img src="/icons/postman.svg" alt="Postman" />
   <img src="/icons/ubuntu.svg" alt="Ubuntu" />
+  <img src="/icons/docker.svg" alt="Docker" />
+  <img src="/icons/artificial-intelligence star.png" alt="Intelligence Artificielle" />
+  <img src="/icons/figma.svg" alt="Figma" />
 </div>
     </section>
   );
