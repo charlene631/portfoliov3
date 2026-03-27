fe446927b19b8588b3ad9fc51f21450e10bb1f2f
Charlène <charlene.dev2025@gmail.com>
2026-03-27 15:03:32 +0100

    mise a jour de section about

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -4,6 +4,9 @@ export default function About() {
   return (
     <section className="section" id="about">
       <h2>À propos de moi</h2>
+      <p>
+  Mon parcours m’a amenée à évoluer dans des environnements exigeants, où la rigueur, l’écoute et la gestion de situations complexes sont essentielles.
+</p>
       <p>Je développe des applications web full stack avec React et Node.js, en portant une attention particulière à la clarté des interfaces, à la sécurité et à l’expérience utilisateur.</p> 
       <p>J’analyse le code en profondeur, j’implémente des tests et j’intègre des pratiques de sécurité dès la conception pour garantir des applications fiables.</p>
       <p>J’utilise également l’intelligence artificielle comme un levier de productivité, tout en gardant un rôle critique sur le code produit.</p>
