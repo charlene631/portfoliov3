a9d2f47884615f459b7bd059c0dbdbfec98770b5
Charlène <charlene.dev2025@gmail.com>
2026-03-29 21:08:41 +0200

    optimisation animation css ambitnflow

--- src/components/AmbientFlow/AmbientFlow.tsx
+++ src/components/AmbientFlow/AmbientFlow.tsx
@@ -4,9 +4,22 @@ export default function AmbientFlow() {
   return (
     <div className="ambient-flow" aria-hidden="true">
       <div className="wave-wrapper">
-        <div className="wave wave1" />
-        <div className="wave wave2" />
-        <div className="wave wave3" />
+
+        <div className="wave wave1">
+          <div className="wave-inner" />
+          <div className="wave-inner" />
+        </div>
+
+        <div className="wave wave2">
+          <div className="wave-inner" />
+          <div className="wave-inner" />
+        </div>
+
+        <div className="wave wave3">
+          <div className="wave-inner" />
+          <div className="wave-inner" />
+        </div>
+
       </div>
     </div>
   );
