52919c8dee159a77a84ee7de5022f929c15c3924
Charlène <charlene.dev2025@gmail.com>
2026-04-13 21:55:35 +0200

    ajustement css section about

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -4,10 +4,9 @@ export default function About() {
   return (
     <section className="section" id="about">
       <h2>À propos de moi</h2>
-      <p>Attirée par le numérique et l'informatique, je me suis récemment formée à l’administration système sous Windows Server 2019. Ce projet m’a permis de développer des compétences clés en gestion d’infrastructure, que je souhaite mettre en pratique dans le cadre d’une alternance en tant que technicienne informatique</p>
-      <p>J’ai finalisé une formation de développeur en 2025, ce qui m’a apporté des bases en logique informatique, compréhension des systèmes et outils numériques. Je m’oriente aujourd’hui vers les métiers du support et des infrastructures, avec un intérêt pour le diagnostic, la résolution de problèmes techniques et l’accompagnement des utilisateurs.</p> 
-      <p>Mon parcours en milieu hospitalier en soins infirmiers (8 ans) m’a permis de développer rigueur, gestion des priorités et une forte expérience de la relation humaine dans des environnements exigeants.</p>
-      <p>J’utilise également l’intelligence artificielle comme un levier de productivité, tout en gardant un rôle critique sur le code produit.</p>
+      <p>Attirée par le numérique et l'informatique, je me suis très récemment formée à l’administration système sous Windows Server 2019. Ce projet m’a permis de développer des compétences clés en gestion d’infrastructure, que je souhaite mettre en pratique dans le cadre d’une alternance en tant que technicienne informatique.</p>
+      <p>J’ai finalisé une formation de développeur en 2025, ce qui m’a apporté des bases en logique informatique, compréhension des systèmes et outils numériques. Je m’oriente aujourd’hui vers les métiers du support et des infrastructures.</p> 
+      <p>Mon parcours en soins infirmiers (8 ans) m’a permis de développer rigueur, gestion des priorités et une forte expérience de la relation humaine dans des environnements exigeants.</p>
       <div className="stack">
         <img src="/icons/node.svg" alt="Node" />
         <img src="/icons/react.svg" alt="React" />
